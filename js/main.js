583a954e66a169ccc7d96492af3f66ebbc8dff91
Dennis <oba.dennis@gmail.com>
2020-06-09 09:42:34 -0400

    changing pattern timings

--- js/main.js
+++ js/main.js
@@ -8,9 +8,15 @@ function animate(){
 
    var tl = new TimelineMax({repeat: -1})
    tl
-   .fromTo("#out-circle, #thin-circle", .25, {drawSVG: "0 0"}, {drawSVG: "0 15%", ease: easeNone})
-   .to("#out-circle, #thin-circle", 1, {drawSVG:"85% 100%", ease: easeNone})
-   .to("#out-circle, #thin-circle", .25, {drawSVG: "100% %", ease: easeNone})
+   .fromTo("#out-circle", .15, {drawSVG: "0 0"}, {drawSVG: "15% 0%", ease: easeNone})
+   .to("#out-circle", 1, {drawSVG:"85% 100%", ease: easeNone})
+   .to("#out-circle", .15, {drawSVG: "100% %", ease: easeNone})
+
+   var tl3 = new TimelineMax({ repeat: -1 })
+   tl3
+      .fromTo("#thin-circle", .15, { drawSVG: "100% 100%" }, { drawSVG: "85% 100%", ease: easeNone })
+      .to("#thin-circle", 1, { drawSVG: "0% 15%", ease: easeNone })
+      .to("#thin-circle", .15, { drawSVG: "0% 0%", ease: easeNone })
 
 
    var tl2 = new TimelineMax
